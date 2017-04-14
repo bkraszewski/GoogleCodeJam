86363ae933b7cedd0a3eb63c2ac0fdf176e86870
Bartosz Kraszewski <bkraszewski@gmail.com>
2017-04-14 23:07:02 +0200

    supporting larger dataset

--- src/com/bkraszewski/BathroomStalls.java
+++ src/com/bkraszewski/BathroomStalls.java
@@ -7,18 +7,18 @@ public class BathroomStalls {
 
     public static Comparator<Cabin> getComparator() {
         return (o1, o2) -> {
-            int diff1 = o1.stop - o1.start;
-            int diff2 = o2.stop - o2.start;
+            long diff1 = o1.stop - o1.start;
+            long diff2 = o2.stop - o2.start;
 
             if (diff1 == diff2) {
-                return o1.start - o2.start;
+                return (int)(o1.start - o2.start);
             }
 
-            return diff2 - diff1;
+            return (int)(diff2 - diff1);
         };
     }
 
-    public static Cabin findLastOccupiedCabin(int numberOfStalls, int queueLenght) {
+    public static Cabin findLastOccupiedCabin(long numberOfStalls, long queueLenght) {
 
         Cabin cabin = new Cabin(0, numberOfStalls - 1);
         PriorityQueue<Cabin> cabins = new PriorityQueue<>(getComparator());
@@ -32,7 +32,7 @@ public class BathroomStalls {
             if (cabin.stop - cabin.start == 0) {
                 //nothing to do, only place in range occupied
             } else {
-                int current = cabin.start + (cabin.stop - cabin.start) / 2;
+                long current = cabin.start + (cabin.stop - cabin.start) / 2;
                 Cabin left = new Cabin(cabin.start, current - 1);
                 Cabin right = new Cabin(current + 1, cabin.stop);
 
@@ -53,12 +53,12 @@ public class BathroomStalls {
 
 
     public static class Cabin {
-        private int start;
-        private int stop;
+        private long start;
+        private long stop;
         public long maxLsRs = 0;
         public long minLsRs = 0;
 
-        public Cabin(int start, int stop) {
+        public Cabin(long start, long stop) {
             this.start = start;
             this.stop = stop;
         }
@@ -72,10 +72,10 @@ public class BathroomStalls {
         }
 
         public void setupInternal() {
-            int ls;
-            int rs;
+            long ls;
+            long rs;
 
-            int mid = start + (stop - start) / 2;
+            long mid = start + (stop - start) / 2;
 
             ls = mid - start;
             rs = stop - mid;
@@ -88,7 +88,7 @@ public class BathroomStalls {
 
     public static void main(String args[]) throws IOException {
 //        String fileName = "B-small-practice";
-        String fileName = "C-small-practice-2";
+        String fileName = "C-large-practice";
 
         String inputFile = String.format("./input/bathrooms/%s.in", fileName);
         String outputFile = String.format("./output/bathrooms/%s.out", fileName);
@@ -97,12 +97,12 @@ public class BathroomStalls {
         reader.mapInputToOutput(inputFile, outputFile, (line) ->
                 {
                     String[] params = line.split("\\s+");
-                    int stalls = Integer.valueOf(params[0]);
-                    int queue = Integer.valueOf(params[1]);
+                    long stalls = Long.valueOf(params[0]);
+                    long queue = Long.valueOf(params[1]);
 
                     Cabin cabin = findLastOccupiedCabin(stalls, queue);
                     String result = String.format("%d %d", cabin.maxLsRs, cabin.minLsRs);
-                    //System.out.println(result);
+                    System.out.println(result);
                     return result;
                 }
         );
